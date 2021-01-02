4400d621e4b8203b31db889a456fc80ad50d2fd7
E H Murdoch <ehmurdoch47@protonmail.com>
2021-01-02 17:36:24 +0000

    Added comments to 'main.js'; 'display' takes into account the state of the checkbox; factored out some constants

--- main.js
+++ main.js
@@ -1,34 +1,52 @@
 const LIST_CONTAINER = document.getElementById ( 'list-container' );
-const CHECKBOX = "<input type='checkbox' />"
 
+const CHECKBOX_UNCHECKED = "<input type='checkbox' />"
+const CHECKBOX_CHECKED = "<input type='checkbox' checked/>"
+
+// so we can have buttons that make function calls specific to the associated item
+const DELETE_BUTTON_PREFIX = `<input type="button" value="Delete" onclick="Items.deleteItem ( '`
+const DELETE_BUTTON_SUFFIX = `' )" />`
+
+// My attempt at the Modular Design Pattern
 let Items = (
     function () {
+        // These variables are private
         let list = [];
-        let reference_counter = -1;
+        // used in the unique ID when an item is created
+        const ITEM_ID_PREFIX = 'item-';
+        let item_id_counter = -1;
+
+        // METHODS ----------------------------------------------
 
         function createItem ( text, completed = false ) {
-            let id = 'item-' + ( ++ reference_counter );
+            // makes sure the items have unique IDs
+            let id = ITEM_ID_PREFIX + ( ++ item_id_counter );
 
+            function display () {
+                // displays the right checkbox
+                let checkbox = completed ? CHECKBOX_CHECKED : CHECKBOX_UNCHECKED;
+
+                let node = document.createElement ( 'li' );
+                node.id = id;
+                node.innerHTML = checkbox + text + DELETE_BUTTON_PREFIX + id + DELETE_BUTTON_SUFFIX;
+
+                LIST_CONTAINER.appendChild ( node );
+            }
+
+            // FUTURE: could factor 'display' out as a main method of the Items module
             list.push (
                 {
                     id: id,
                     text: text,
                     completed: false,
-                    display: function () {
-                        let node = document.createElement ( 'li' );
-                        node.id = id;
-                        node.innerHTML = `${CHECKBOX}${text}<input type='button' value='Delete' onclick="Items.deleteItem ( '${id}' )" />`
-
-                        LIST_CONTAINER.appendChild ( node );
-                    }
+                    display: display
                 }
             );
-
-            return id;
         }
 
         function deleteItem ( item_id ) {
             LIST_CONTAINER.removeChild ( document.getElementById ( item_id ) );
+
             list = list.filter ( ({ id }) => id != item_id );
         }
 
@@ -40,6 +58,7 @@ let Items = (
             createItem: createItem,
             displayAll: displayAll,
             deleteItem: deleteItem,
+            // only for debugging purposes
             printItems: () => { console.log ( list ); }
         }
     }
@@ -47,11 +66,11 @@ let Items = (
 
 
 document.body.onload = function () {
-    Items.createItem ( 'item 1' );
-    Items.createItem ( 'item 2' );
-    Items.createItem ( 'item 3' );
-    Items.createItem ( 'item 4' );
-    Items.createItem ( 'item 5' );
+    Items.createItem ( 'item 1'       );
+    Items.createItem ( 'item 2', true );
+    Items.createItem ( 'item 3'       );
+    Items.createItem ( 'item 4'       );
+    Items.createItem ( 'item 5', true );
 
     Items.displayAll ();
 }
