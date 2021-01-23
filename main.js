bb7f1f3725392dd8f7d5db359b238abcb0c588ea
E H Murdoch <ehmurdoch47@protonmail.com>
2021-01-23 10:53:27 +0000

    added background; now uses DOM methods instead of 'innerHTML'; added fonts

--- main.js
+++ main.js
@@ -5,19 +5,81 @@ let Items = (
         const LIST_CONTAINER = document.getElementById ( 'list-container' );
         const ITEM_TEXT_INPUT = document.getElementById ( 'item-text-input' );
 
-        const CHECKBOX_UNCHECKED = "<input type='checkbox' />";
-        const CHECKBOX_CHECKED = "<input type='checkbox' checked/>";
-
-        // these two constants are so we can have buttons that make function calls specific to the associated item
-        const DELETE_BUTTON_PREFIX = `<input type="button" value="Delete" onclick="Items.deleteItem ( '`;
-        const DELETE_BUTTON_SUFFIX = `' )" />`;
-
         let list = [];
         // used in the unique ID when an item is created
         const ITEM_ID_PREFIX = 'item-';
         let item_id_counter = -1;
 
         // METHODS ----------------------------------------------
+
+        //     DOM METHODS --------------------------------------
+
+        function createDOMStructureForItem ( item ) {
+            let list_item_element = createListItemForItem ( item );
+            let label_element     = createLabelForItem    ( item );
+
+            label_element.appendChild ( createTextElementForItem   ( item ) );
+            label_element.appendChild ( createCheckboxForItem      ( item ) );
+            // FUTURE: factor this out as a constant
+            label_element.appendChild ( createCheckmarkSpanForItem (      ) );
+
+            list_item_element.appendChild ( label_element                      );
+            list_item_element.appendChild ( createDeleteButtonForItem ( item ) );
+
+            return list_item_element;
+        }
+
+        function createListItemForItem ( item ) {
+            let element = document.createElement ( 'li' );
+
+            element.setAttribute ( 'id'   ,  item.id  );
+            element.setAttribute ( 'class', 'item'    );
+
+            return element;
+        }
+
+        function createTextElementForItem ( item ) {
+            return document.createTextNode ( item.text );
+        }
+
+        function createLabelForItem ( item ) {
+            let element = document.createElement ( 'label' );
+
+            element.setAttribute ( 'class', 'checkbox-container' );
+
+            return element;
+        }
+
+        function createCheckboxForItem ( item ) {
+            let element = document.createElement ( 'input' );
+
+            element.setAttribute ( 'type', 'checkbox');
+            if ( item.completed ) {
+                element.setAttribute ( 'checked', '' );
+            }
+
+            return element;
+        }
+
+        function createCheckmarkSpanForItem () {
+            let checkmark_span_element = document.createElement ( 'span' );
+
+            checkmark_span_element.setAttribute ( 'class', 'checkmark' );
+
+            return checkmark_span_element;
+        }
+
+        function createDeleteButtonForItem ( item ) {
+            let delete_button_element = document.createElement ( 'input' );
+
+            delete_button_element.setAttribute ( 'type'   , 'button'                            );
+            delete_button_element.setAttribute ( 'value'  , 'Delete'                            );
+            delete_button_element.setAttribute ( 'onclick', `Items.deleteItem ( '${item.id}' )` );
+
+            return delete_button_element;
+        }
+
+        //     METHODS FOR ITEMS --------------------------------
 
         function createItem ( text, completed = false ) {
             // makes sure the items have unique IDs
@@ -37,14 +99,9 @@ let Items = (
         function displayItem ( item ) {
             // prevents having elements with the same id
             if ( document.getElementById ( item.id ) == null ) {
-                // displays the right checkbox
-                let checkbox = item.completed ? CHECKBOX_CHECKED : CHECKBOX_UNCHECKED;
+                let item_node = createDOMStructureForItem ( item );
 
-                let node = document.createElement ( 'li' );
-                node.id = item.id;
-                node.innerHTML = checkbox + item.text + DELETE_BUTTON_PREFIX + item.id + DELETE_BUTTON_SUFFIX;
-
-                LIST_CONTAINER.appendChild ( node );
+                LIST_CONTAINER.appendChild ( item_node );
             }
         }
 
